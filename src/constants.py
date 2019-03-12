bd15eeb3a3449342fdbf611e3c882a0ba5f92a89
RePierre <petru.rebeja@gmail.com>
2019-03-12 14:21:45 +0200

    Removed constants file.

--- src/constants.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-
-EMBEDDING_DIM = 10
-SAMPLE_DIM = 3
-LATENT_DIM = 128
-SEQ_START = np.array([1] + [0] * (EMBEDDING_DIM - 1))
-SEQ_END = np.array([0] + [1] + [0] * (EMBEDDING_DIM - 2))
-MARK = np.array([0, 0, 1] + [0] * (EMBEDDING_DIM - 3))
